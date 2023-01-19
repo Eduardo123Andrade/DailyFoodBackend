da533b7a6eafb859320e9ab91cbc05b418ae910f
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-01-19 20:47:40 -0300

    Fix - created alias

--- mix.exs
+++ mix.exs
@@ -53,8 +53,10 @@ defmodule Dailyfood.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
 
-      # Development
+      # Development and Test
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+
+      # Development
       {:pbkdf2_elixir, "~> 1.3"},
       {:ex_machina, "~> 2.7.0"},
       {:tesla, "~> 1.4"},
