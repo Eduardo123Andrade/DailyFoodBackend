adacac8bbfbfd0b940b66e3d91c0c9c23944f470
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-02-28 18:48:53 -0300

    Test - created food test

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,6 +1,8 @@
 defmodule Dailyfood.Factory do
-  alias Dailyfood.Users.User
   use ExMachina.Ecto, repo: Dailyfood.Repo
+
+  alias Dailyfood.Foods.Food
+  alias Dailyfood.Users.User
 
   def user_params_factory do
     %{
@@ -16,6 +18,22 @@ defmodule Dailyfood.Factory do
       password: "123123123",
       name: "Stark",
       id: "957da868-ce7f-4eec-bcdc-97b8c992a60d"
+    }
+  end
+
+  def food_params_factory do
+    %{
+      "description" => "Arroz",
+      "weight" => 100
+    }
+  end
+
+  def food_factory do
+    %Food{
+      description: "Arroz",
+      weight: 100,
+      id: "8bb494a5-cae7-4027-845f-5b2ba10c6677",
+      meal_id: "11d9c69d-e05f-4952-8637-44fb7b3010a1"
     }
   end
 end
