3311d829aaeda4030bf61ddff36506e2d495504c
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-03-21 20:09:43 -0300

    Feat - created authenticate function

--- lib/dailyfood_web/auth/guardian.ex
+++ lib/dailyfood_web/auth/guardian.ex
@@ -1,4 +1,5 @@
 defmodule DailyfoodWeb.Auth.Guardian do
+  alias Dailyfood.Error
   alias Dailyfood.Users.User
   alias Dailyfood.Users.Get, as: UserGet
   use Guardian, otp_app: :dailyfood
@@ -10,4 +11,17 @@ defmodule DailyfoodWeb.Auth.Guardian do
     |> Map.get("sub")
     |> UserGet.by_id()
   end
+
+  def authenticate(%{"email" => email, "password" => password}) do
+    with {:ok, %User{password_hash: hash} = user} <- Dailyfood.get_user_by_email(email),
+         true <- Pbkdf2.verify_pass(password, hash),
+         {:ok, token, _claims} <- encode_and_sign(user) do
+      {:ok, user, token}
+    else
+      false -> {:error, Error.build(:unauthorized, "Please verify your credentials")}
+      error -> error
+    end
+  end
+
+  def authenticate(_), do: {:error, Error.build(:bad_request, "Invalid or missing params")}
 end
