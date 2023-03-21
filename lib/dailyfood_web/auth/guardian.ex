72eac5446e79ef6b2b74cbd7a46603168c034e95
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-03-21 09:30:11 -0300

    Feat - Removed IO.inspect

--- lib/dailyfood_web/auth/guardian.ex
+++ lib/dailyfood_web/auth/guardian.ex
@@ -3,10 +3,7 @@ defmodule DailyfoodWeb.Auth.Guardian do
   alias Dailyfood.Users.Get, as: UserGet
   use Guardian, otp_app: :dailyfood
 
-  def subject_for_token(%User{id: id}, _claims) do
-    IO.inspect(id, label: "subject_for_token")
-    {:ok, id}
-  end
+  def subject_for_token(%User{id: id}, _claims), do: {:ok, id}
 
   def resource_from_claims(claims) do
     claims
